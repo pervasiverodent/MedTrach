657e958b99dd343c1b97434024176a28cedfa620
pervasiverodent <dhustehez@gmail.com>
2020-09-17 20:38:48 +0800

    Added Sign Up activity.

--- app/src/main/java/com/java/medtrach/SignUpActivity.java
+++ app/src/main/java/com/java/medtrach/SignUpActivity.java
@@ -24,117 +24,106 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.java.medtrach.R;
 import com.java.medtrach.common.Common;
+import com.java.medtrach.common.LoadingAnimation;
+import com.java.medtrach.common.ValidateInput;
 import com.java.medtrach.model.User;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private EditText usernameEditText, passwordEditText, confirmPasswordEditText;
-    private TextView loginRedirectTextView;
-    private Button signUpButton;
+    ValidateInput validateInput;
+    LoadingAnimation loadingAnimation;
+    User userModel;
+
+    EditText emailEditText, passwordEditText, confirmPasswordEditText;
+    TextView loginRedirectTextView;
+    Button signUpButton;
 
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
     private FirebaseDatabase mDatabase;
     private DatabaseReference userReference;
 
-    String userId, username, password, confirmPassword;
+    String userId, email, password, confirmPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        initializeContent();
-        initializeFirebase();
-
-
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkUserInput();
-            }
-        });
-    }
-
-    private void initializeContent() {
-        usernameEditText = findViewById(R.id.sign_up_username);
+        emailEditText = findViewById(R.id.sign_up_username);
         passwordEditText = findViewById(R.id.sign_up_password);
         confirmPasswordEditText = findViewById(R.id.sign_up_confirm_password);
         loginRedirectTextView = findViewById(R.id.sign_up_login_text_view);
         signUpButton = findViewById(R.id.sign_up_button);
 
+        // Loading animation
+        loadingAnimation = new LoadingAnimation(SignUpActivity.this);
+
         mDatabase = FirebaseDatabase.getInstance();
         mUser = FirebaseAuth.getInstance().getCurrentUser();
         mAuth = FirebaseAuth.getInstance();
         userReference = FirebaseDatabase.getInstance().getReference(Common.USER_REF);
+
+
+        validateInput = new ValidateInput(
+            SignUpActivity.this, emailEditText, passwordEditText, confirmPasswordEditText
+        );
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerNewAccount();
+            }
+        });
     }
 
-    private void initializeFirebase() {
+    private void registerNewAccount() {
 
-    }
+        loadingAnimation.LoadingAnimationDialog();
 
-    private void checkUserInput() {
-        final String email = usernameEditText.getText().toString().trim();
-        final String password = passwordEditText.getText().toString().trim();
-        final String confirmPassword = confirmPasswordEditText.getText().toString().trim();
+        boolean emailVerified = validateInput.validateEmail();
+        boolean passwordVerified = validateInput.validatePassword();
+        boolean confirmPasswordVerified = validateInput.validateConfirmPassword();
 
-        if (TextUtils.isEmpty(email)) {
-            Toast.makeText(this, "Username must not be empty.", Toast.LENGTH_SHORT).show();
-        }
+        if(emailVerified && passwordVerified && confirmPasswordVerified) {
+            email = emailEditText.getText().toString().trim();
+            password = passwordEditText.getText().toString().trim();
 
-        if(TextUtils.isEmpty(password)) {
-            Toast.makeText(this, "Password must not be empty.", Toast.LENGTH_SHORT).show();
-        }
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
 
-        if(TextUtils.isEmpty(confirmPassword)) {
-            Toast.makeText(this, "Confirm password must not be empty.", Toast.LENGTH_SHORT).show();
-        }
+                        // Firebase
+                        mUser = FirebaseAuth.getInstance().getCurrentUser();
+                        String userId = mUser.getUid();
 
-        if(password.length() <= 6) {
-            Toast.makeText(this, "Password length cannot be less than six characters.", Toast.LENGTH_SHORT).show();
-        }
+                        userModel.setUid(userId);
+                        userModel.setUsername(email);
+                        userModel.setPassword(password);
 
-        if(!password.equals(confirmPassword)) {
-            Toast.makeText(this, "Please check your password fields.", Toast.LENGTH_SHORT).show();
-        }
+                        userReference.child(userId).setValue(userModel);
 
-        mAuth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    mUser = FirebaseAuth.getInstance().getCurrentUser();
-                    String userId = mUser.getUid();
+                        if(task.isSuccessful()) {
 
-                    User user = new User();
-
-                    user.setUid(userId);
-                    user.setUsername(username);
-                    user.setPassword(password);
-
-                    userReference.child(userId).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-
+                            // Redirect User to Home
+                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            //Loading animation
+                            loadingAnimation.dismissLoadingAnimation();
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Fatal Error", Toast.LENGTH_SHORT).show();
+                            loadingAnimation.dismissLoadingAnimation();
                         }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(SignUpActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-                    if(!task.isSuccessful()) {
-                        Toast.makeText(SignUpActivity.this, "Register failed.", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(SignUpActivity.this, "User has successfully registered!", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                        startActivity(intent);
                     }
+                });
 
-                }
-            });
-
-
+        } else {
+            loadingAnimation.dismissLoadingAnimation();
+        }
     }
 
 
---
+++ app/src/main/java/com/java/medtrach/common/ValidateInput.java
@@ -0,0 +1,76 @@
+package com.java.medtrach.common;
+
+import android.content.Context;
+import android.util.Patterns;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ValidateInput {
+    Context context;
+    EditText email, password, confirmPassword;
+
+    String emailInput, passwordInput, confirmPasswordInput;
+
+    /**
+     * SignInActivity.class
+     */
+
+    ValidateInput(Context myContext, EditText myEmail, EditText myPassword) {
+        context = myContext;
+        email = myEmail;
+        password = myPassword;
+    }
+
+    /**
+     * SignUpActivity.class
+     */
+
+    public ValidateInput(Context myContext, EditText myEmail, EditText myPassword, EditText myConfirmPassword) {
+        context = myContext;
+        email = myEmail;
+        password = myPassword;
+        confirmPassword = myConfirmPassword;
+    }
+
+    public boolean validateEmail() {
+        emailInput = email.getText().toString().trim();
+
+        if(emailInput.isEmpty()) {
+            Toast.makeText(context, "Your email address is empty.", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            Toast.makeText(context, "Invalid email address input.", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean validatePassword() {
+        passwordInput = password.getText().toString().trim();
+
+        if(passwordInput.isEmpty()) {
+            Toast.makeText(context, "Please enter your password.", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (passwordInput.length() < 8) {
+            Toast.makeText(context, "Password is too short. Less than 8 characters", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean validateConfirmPassword() {
+        confirmPasswordInput = confirmPassword.getText().toString().trim();
+
+        if(confirmPasswordInput.isEmpty()) {
+            Toast.makeText(context, "Please fill out all fields.", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!confirmPasswordInput.equals(passwordInput)) {
+            Toast.makeText(context, "Passwords dont match.", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
